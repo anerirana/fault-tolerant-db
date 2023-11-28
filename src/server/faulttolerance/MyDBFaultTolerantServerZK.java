a397516af47ca3afa05963d31d88185dc49574d5
Neel Ritesh Yadav <neelriteshyadav@vl965-172-31-117-149.wireless.umass.edu>
2023-11-28 18:16:48 -0500

    base code

--- src/server/faulttolerance/MyDBFaultTolerantServerZK.java
+++ src/server/faulttolerance/MyDBFaultTolerantServerZK.java
@@ -1,14 +1,41 @@
 package server.faulttolerance;
 
+import edu.umass.cs.nio.AbstractBytePacketDemultiplexer;
+import edu.umass.cs.nio.MessageNIOTransport;
 import edu.umass.cs.nio.interfaces.NodeConfig;
 import edu.umass.cs.nio.nioutils.NIOHeader;
 import edu.umass.cs.nio.nioutils.NodeConfigUtils;
 import edu.umass.cs.utils.Util;
-import server.AVDBReplicatedServer;
 import server.ReplicatedServer;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.logging.Level;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+import client.AVDBClient;
+import client.MyDBClient;
+
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooDefs;
+import org.apache.zookeeper.AddWatchMode;
+
+import java.util.logging.Logger;
+import java.net.InetAddress;
+import java.util.List;
+
 
 /**
  * This class should implement your replicated fault-tolerant database server if
@@ -55,6 +82,16 @@ public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer {
 	public static final int MAX_LOG_SIZE = 400;
 
 	public static final int DEFAULT_PORT = 2181;
+	
+	private static final Logger LOGGER = Logger.getLogger(MyDBFaultTolerantServerZK.class.getName());
+	
+	private final ZooKeeper zk;
+	final private Session session;
+    final private Cluster cluster;
+
+    protected final String myID;
+    protected final MessageNIOTransport<String,String> serverMessenger;
+
 
 	/**
 	 * @param nodeConfig Server name/address configuration information read
@@ -72,7 +109,30 @@ public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer {
 		super(new InetSocketAddress(nodeConfig.getNodeAddress(myID),
 				nodeConfig.getNodePort(myID) - ReplicatedServer
 						.SERVER_PORT_OFFSET), isaDB, myID);
+		session = (cluster=Cluster.builder().addContactPoint("127.0.0.1")
+                .build()).connect(myID);
+		log.log(Level.INFO, "Server {0} added cluster contact point", new Object[]{myID,});
 
+		this.myID = myID;
+
+		this.serverMessenger =  new MessageNIOTransport<String, String>(myID, nodeConfig, new AbstractBytePacketDemultiplexer() {
+                            @Override
+                            public boolean handleMessage(byte[] bytes, NIOHeader nioHeader) {
+                                handleMessageFromServer(bytes, nioHeader);
+                                return true;
+                            }
+                        }, true);
+
+		log.log(Level.INFO, "Server {0} started on {1}", new Object[]{this.myID, this.clientMessenger.getListeningSocketAddress()});
+		
+		this.zk = new ZooKeeper("localhost:" + DEFAULT_PORT, 3000, new Watcher() {
+            @Override
+            public void process(WatchedEvent event) {
+                if (event.getType() == Watcher.Event.EventType.NodeCreated) {
+                	System.out.println("Children data changed.");
+                }
+            }
+		});
 		// TODO: Make sure to do any needed crash recovery here.
 	}
 
@@ -100,6 +160,31 @@ public class MyDBFaultTolerantServerZK extends server.MyDBSingleServer {
 
 	public static enum CheckpointRecovery {
 		CHECKPOINT, RESTORE;
+	}
+
+	public void trimLogs() {
+	    try {
+	        // Get the list of children from ZooKeeper
+	        List<String> children = zk.getChildren("/requests", false);
+
+	        // Check if there are at least 400 elements
+	        if (children.size() >= 400) {
+	            // Get the sublist of the first 100 elements
+	            List<String> sublist = children.subList(0, 100);
+
+	            // Remove the first 100 elements from the list
+	            children.removeAll(sublist);
+
+	            // Delete the corresponding nodes from ZooKeeper
+	            for (String node : sublist) {
+	                String path = "/requests/" + node;
+	                zk.delete(path, -1);
+	            }
+	        }
+	    } catch (KeeperException | InterruptedException e) {
+	        e.printStackTrace();
+	        // Handle exceptions appropriately
+	    }
 	}
 
 	/**
